cfc5384c4ceb3a292454549923275463308c1000
Colum Kelly <columk1@users.noreply.github.com>
2026-03-12 22:11:52 -0700

    feat: add missing type declaration

---
+++ src/types/rehype-figure-title.d.ts
@@ -0,0 +1,11 @@
+declare module "rehype-figure-title" {
+  import { Plugin } from "unified";
+
+  interface RehypeFigureTitleOptions {
+    // Add any options if they exist
+  }
+
+  const rehypeFigureTitle: Plugin<[RehypeFigureTitleOptions?], any, any>;
+
+  export = rehypeFigureTitle;
+}
